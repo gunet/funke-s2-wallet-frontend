5e06622489ecdfaf9073568f93b27828cc4c5e47
kkmanos <koukoularisman@gmail.com>
2024-10-31 14:54:35 +0200

    connect with eid-client

--- src/hooks/useContainer.ts
+++ src/hooks/useContainer.ts
@@ -193,6 +193,30 @@ export function useContainer() {
 					...c
 				});
 			},
+			async function authorizationRequestModifier(credentialIssuerIdentifier: string, url: string, request_uri?: string, client_id?: string) {
+				if (credentialIssuerIdentifier !== process.env.REACT_APP_PID_CREDENTIAL_ISSUER_IDENTIFIER) {
+					return { url };
+				}
+				const isMobile = window.innerWidth <= 480;
+				const eIDClientURL = isMobile ? process.env.REACT_APP_OPENID4VCI_EID_CLIENT_URL.replace('http', 'eid') : process.env.REACT_APP_OPENID4VCI_EID_CLIENT_URL;
+				console.log("Eid client url = ", eIDClientURL)
+				const urlObj = new URL(url);
+				// Construct the base URL
+				const baseUrl = `${urlObj.protocol}//${urlObj.hostname}${urlObj.pathname}`;
+		
+				// Parameters
+				// Encode parameters
+				const encodedClientId = encodeURIComponent(client_id);
+				const encodedRequestUri = encodeURIComponent(request_uri);
+				const tcTokenURL = `${baseUrl}?client_id=${encodedClientId}&request_uri=${encodedRequestUri}`;
+		
+				const newLoc = `${eIDClientURL}?tcTokenURL=${encodeURIComponent(tcTokenURL)}`
+		
+				console.log("new loc = ", newLoc)
+				return {
+					url: newLoc
+				};
+			}
 		);
 
 
