38ad4056b1e540fdcc1d256d8b09cd420ed45892
Georgios Anastasiou <gsiou@gunet.gr>
2024-11-19 10:33:17 +0200

    mdocProtocol: added HKDF shared secret functionality

--- src/lib/utils/mdocProtocol.ts
+++ src/lib/utils/mdocProtocol.ts
@@ -61,3 +61,63 @@ export async function decryptMessage(sessionKey, iv, ciphertext) {
 export function hexToUint8Array(hexString) {
 	return Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
 }
+
+export function uint8ArrayToBase64Url(array: any) {
+	// Convert the Uint8Array to a binary string
+	let binaryString = '';
+	array.forEach((byte: any) => {
+		binaryString += String.fromCharCode(byte);
+	});
+
+	// Convert the binary string to a Base64 string
+	let base64String = btoa(binaryString);
+
+	// Convert the Base64 string to Base64URL format
+	let base64UrlString = base64String
+		.replace(/\+/g, '-') // Replace + with -
+		.replace(/\//g, '_') // Replace / with _
+		.replace(/=+$/, ''); // Remove trailing '='
+
+	return base64UrlString;
+}
+
+export async function deriveSKReader(sessionTranscriptBytes) {
+
+}
+
+/*
+		Source: https://github.com/mdn/dom-examples/blob/main/web-crypto/derive-key/hkdf.js
+    Derive a shared secret, given:
+    - our ECDH private key
+    - their ECDH public key
+*/
+export async function deriveSharedSecret(privateKey, publicKey) {
+	const secret = await crypto.subtle.deriveBits(
+		{ name: "ECDH", public: publicKey },
+		privateKey,
+		256
+	);
+
+	return crypto.subtle.importKey(
+		"raw",
+		secret,
+		{ name: "HKDF" },
+		false,
+		["deriveKey"]
+	);
+}
+
+export async function getKey(keyMaterial, salt, info) {
+	return await crypto.subtle.deriveKey(
+		{
+			name: "HKDF",
+			salt: salt,
+			info: new TextEncoder().encode(info).buffer,
+			hash: "SHA-256",
+		},
+		keyMaterial,
+		{ name: "AES-GCM", length: 256 },
+		true,
+		["encrypt", "decrypt"]
+	);
+}
